d4afa117273da268575ae20e5e2702b89c7ff716
Lyubomir Markov <lyubotopa@gmail.com>
2024-02-23 09:23:25 +0200

    String comparison solution - slow, but works
    * Project structure
    
    * Build up solver - slooooow
    
    * Top down solver - much faster, still slow
    
    * Fix top down solver

---
+++ src/main/java/com/ioncannon/Main.java
@@ -0,0 +1,29 @@
+package com.ioncannon;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class Main {
+    public static void main(String[] args) {
+        FileReader fileReader = new FileReader();
+        ArrayList<String> dictionary = fileReader.readRemoteFile("https://raw.githubusercontent.com/nikiiv/" +
+                "JavaCodingTestOne/master/scrabble-words.txt");
+
+        long beforeSolving = System.currentTimeMillis();
+        TopDownSolver solver = new TopDownSolver();
+        //BuildUpSolver solver = new BuildUpSolver();
+        ArrayList<String[]> solutions = solver.solve(dictionary, new ArrayList<>(Arrays.asList("A", "I")), 9);
+        long afterSolving = System.currentTimeMillis();
+
+        System.out.printf("Solving took %d milliseconds%n", afterSolving-beforeSolving);
+        System.out.println(solutions.size() + " solutions found");
+
+        for (String[] solution : solutions) {
+            System.out.println("\n");
+            for (int i=solution.length-1; i>=0; i--) {
+                System.out.println(solution[i]);
+            }
+        }
+    }
+}
